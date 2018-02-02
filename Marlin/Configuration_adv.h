5a6ebe465201e4bb9e01fb210cb10a3fb7230cd3
tonokip <tonokip@gmail.com>
2018-02-02 11:55:52 -0600

    cfg: basic configuration for FeynmanLight (motherboard,extruders,thermistors,tmc2130)

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1013,14 +1013,14 @@
 #if ENABLED(HAVE_TMC2130)
 
   // CHOOSE YOUR MOTORS HERE, THIS IS MANDATORY
-  #define X_IS_TMC2130
+  //#define X_IS_TMC2130
   //#define X2_IS_TMC2130
-  #define Y_IS_TMC2130
+  //#define Y_IS_TMC2130
   //#define Y2_IS_TMC2130
   #define Z_IS_TMC2130
   //#define Z2_IS_TMC2130
-  #define E0_IS_TMC2130
-  #define E1_IS_TMC2130
+  //#define E0_IS_TMC2130
+  //#define E1_IS_TMC2130
   //#define E2_IS_TMC2130
   //#define E3_IS_TMC2130
   //#define E4_IS_TMC2130
