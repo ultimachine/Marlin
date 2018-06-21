c67906bf269efce62c990eba6814071c5673040f
Marcio Teixeira <marcio@alephobjects.com>
2018-06-21 15:30:01 -0600

    Updated "ArduinoAddons" with licenses.
    - The libraries were replaced with upstream versions for which licenses were available.
    - Added "SOURCES.txt" file.

--- Marlin/Conditionals_LulzBot.h
+++ Marlin/Conditionals_LulzBot.h
@@ -13,7 +13,7 @@
  * got disabled.
  */
 
-#define LULZBOT_FW_VERSION ".59" // Change this with each update
+#define LULZBOT_FW_VERSION ".60" // Change this with each update
 
 #if ( \
     !defined(LULZBOT_Gladiola_Mini) && \
