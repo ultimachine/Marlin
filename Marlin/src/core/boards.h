e20e247afff7a88a29667505a7c4baa8d8dd449e
Tonokip <tonokip@gmail.com>
2020-12-28 14:49:55 -0600

    basic Archimajor pin map

--- Marlin/src/core/boards.h
+++ Marlin/src/core/boards.h
@@ -269,8 +269,9 @@
 #define BOARD_ULTRATRONICS_PRO        3022  // ReprapWorld Ultratronics Pro V1.0
 #define BOARD_ARCHIM1                 3023  // UltiMachine Archim1 (with DRV8825 drivers)
 #define BOARD_ARCHIM2                 3024  // UltiMachine Archim2 (with TMC2130 drivers)
-#define BOARD_ALLIGATOR               3025  // Alligator Board R2
-#define BOARD_CNCONTROLS_15D          3026  // Cartesio CN Controls V15 on DUE
+#define BOARD_ARCHIMAJOR              3025  // UltiMachine Archimajor (with TMC5160 drivers)
+#define BOARD_ALLIGATOR               3026  // Alligator Board R2
+#define BOARD_CNCONTROLS_15D          3027  // Cartesio CN Controls V15 on DUE
 
 //
 // SAM3X8C ARM Cortex M3
